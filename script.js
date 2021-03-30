b4cd69af4d66305b2f3953ca8c302c960a8542ab
Vladimir <37012102+Killarun@users.noreply.github.com>
2021-03-30 14:08:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -222,3 +222,25 @@ function lookUpProfile(name, prop){
 }
 
 lookUpProfile("Akira", "likes");
+
+
+
+///
+class Thermostat {
+  constructor(fahrenheit) {
+    this.fahrenheit = fahrenheit;
+  }
+  
+  get temperature() {
+    return (5 / 9) * (this.fahrenheit - 32);
+  }
+  
+  set temperature(celsius) {
+    this.fahrenheit = (celsius * 9.0) / 5 + 32;
+  }
+}
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
